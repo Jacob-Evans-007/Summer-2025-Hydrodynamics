3e7d98e422abcda4ab30d44cc6d885c7241b12e7
Jacob Evans <jacobgevans922@gmail.com>
2025-07-08 19:22:20 -0400

    many changes, including the great graph!

--- WiersmaCopy.py
+++ WiersmaCopy.py
@@ -1,6 +1,7 @@
 import numpy as np
 import glob, h5py
 from scipy import interpolate
+from numba import njit
 
 class Wiersma_Cooling:
     """
@@ -15,30 +16,33 @@ class Wiersma_Cooling:
         fns = np.array(glob.glob(dataDir + 'CoolingTables/z_?.???.hdf5'))
         zs = np.array([float(fn[-10:-5]) for fn in fns])
         fn = fns[zs.argsort()][searchsortedclosest(sorted(zs), z)]
-        
-        f = h5py.File(fn, 'r')
-        
-        He2Habundance = 10**-1.07 * (0.71553 + 0.28447 * Z2Zsun)
-        X = (1 - 0.014 * Z2Zsun) / (1. + 4. * He2Habundance)
-        Y = 4. * He2Habundance * X
-        iHe = searchsortedclosest(f['Metal_free']['Helium_mass_fraction_bins'][...], Y)
-        
-        H_He_Cooling  = f['Metal_free']['Net_Cooling'][iHe, ...]
-        Tbins         = f['Metal_free']['Temperature_bins'][...]
-        nHbins        = f['Metal_free']['Hydrogen_density_bins'][...]
-        Metal_Cooling = f['Total_Metals']['Net_cooling'][...] * Z2Zsun
-        
-        # Final cooling table
-        Cooling_Table = Metal_Cooling + H_He_Cooling
 
-        # Store interpolator
+        with h5py.File(fn, 'r') as f:
+            He2Habundance = 10**-1.07 * (0.71553 + 0.28447 * Z2Zsun)
+            X = (1 - 0.014 * Z2Zsun) / (1. + 4. * He2Habundance)
+            Y = 4. * He2Habundance * X
+            iHe = searchsortedclosest(f['Metal_free']['Helium_mass_fraction_bins'][...], Y)
+
+            H_He_Cooling = f['Metal_free']['Net_Cooling'][iHe, ...]
+            Tbins = f['Metal_free']['Temperature_bins'][...]
+            nHbins = f['Metal_free']['Hydrogen_density_bins'][...]
+            Metal_Cooling = f['Total_Metals']['Net_cooling'][...] * Z2Zsun
+
+            Cooling_Table = Metal_Cooling + H_He_Cooling
+
+        # Store grids and table in log space for interpolation
+        self.logT_grid = np.log10(Tbins)
+        self.logn_grid = np.log10(nHbins)
+        self.logLambda_table = np.log10(Cooling_Table)
+
+        # For fallback (non-numba) smooth interpolation if needed
         self.f_Cooling = interpolate.RegularGridInterpolator(
-            (np.log10(Tbins), np.log10(nHbins)),
+            (self.logT_grid, self.logn_grid),
             Cooling_Table,
             bounds_error=False, fill_value=None
         )
 
-        # Store derivatives
+        # Compute derivatives for gradient interpolation (optional)
         Xg, Yg = np.meshgrid(Tbins, nHbins, indexing='ij')
         log_vals = np.log(self.f_Cooling((np.log10(Xg), np.log10(Yg))))
         dlogT = np.diff(np.log10(Tbins))[0]
@@ -46,24 +50,61 @@ class Wiersma_Cooling:
         dlnLambda_dlnrhoArr, dlnLambda_dlnTArr = np.gradient(log_vals, dlogn, dlogT)
 
         self.dlnLambda_dlnT_interp = interpolate.RegularGridInterpolator(
-            (np.log10(Tbins), np.log10(nHbins)),
+            (self.logT_grid, self.logn_grid),
             dlnLambda_dlnTArr,
             bounds_error=False, fill_value=None
         )
         self.dlnLambda_dlnrho_interp = interpolate.RegularGridInterpolator(
-            (np.log10(Tbins), np.log10(nHbins)),
+            (self.logT_grid, self.logn_grid),
             dlnLambda_dlnrhoArr,
             bounds_error=False, fill_value=None
         )
 
+    @staticmethod
+    @njit
+    def fast_LAMBDA_numba(T, nH, logT_grid, logn_grid, logLambda_table):
+        logT = np.log10(T)
+        logn = np.log10(nH)
+
+        i = np.searchsorted(logT_grid, logT) - 1
+        j = np.searchsorted(logn_grid, logn) - 1
+
+        i = max(0, min(i, len(logT_grid) - 2))
+        j = max(0, min(j, len(logn_grid) - 2))
+
+        x1 = logT_grid[i]
+        x2 = logT_grid[i + 1]
+        y1 = logn_grid[j]
+        y2 = logn_grid[j + 1]
+
+        Q11 = logLambda_table[i, j]
+        Q12 = logLambda_table[i, j + 1]
+        Q21 = logLambda_table[i + 1, j]
+        Q22 = logLambda_table[i + 1, j + 1]
+
+        denom = (x2 - x1) * (y2 - y1)
+        if denom == 0:
+            return 10 ** Q11
+
+        f = (
+            Q11 * (x2 - logT) * (y2 - logn) +
+            Q21 * (logT - x1) * (y2 - logn) +
+            Q12 * (x2 - logT) * (logn - y1) +
+            Q22 * (logT - x1) * (logn - y1)
+        ) / denom
+
+        return 10 ** f
+
+    def fast_LAMBDA(self, T, nH):
+        return self.fast_LAMBDA_numba(T, nH, self.logT_grid, self.logn_grid, self.logLambda_table)
+
     def LAMBDA(self, T, nH):
-        """Cooling function in erg cm^3 / s"""
+        # fallback to scipy interpolator if you want
         return self.f_Cooling((np.log10(T), np.log10(nH)))
 
     def tcool(self, T, nH):
-        """Cooling time in seconds (uses 3.5 k_B T / (nH * Lambda))"""
         k_B = 1.380649e-16  # erg/K
-        Lambda_val = self.LAMBDA(T, nH)
+        Lambda_val = self.fast_LAMBDA(T, nH)  # use fast here for speed
         return (3.5 * k_B * T) / (nH * Lambda_val)
 
     def f_dlnLambda_dlnT(self, T, nH):
@@ -79,20 +120,3 @@ def searchsortedclosest(arr, val):
     left = arr[ind - 1]
     right = arr[ind]
     return ind - 1 if abs(val - left) < abs(val - right) else ind
-    
-
-
-
-def searchsortedclosest(arr, val):
-    if arr[0]<arr[1]:
-        ind = np.searchsorted(arr,val)
-        ind = minarray(ind, len(arr)-1)
-        return maxarray(ind - (val - arr[maxarray(ind-1,0)] < arr[ind] - val),0)        
-    else:
-        ind = np.searchsorted(-arr,-val)
-        ind = minarray(ind, len(arr)-1)
-        return maxarray(ind - (-val + arr[maxarray(ind-1,0)] < -arr[ind] + val),0)        
-def maxarray(arr, v):
-    return arr + (arr<v)*(v-arr)
-def minarray(arr, v):
-    return arr + (arr>v)*(v-arr)
--- lab_functions_1.py
+++ lab_functions_1.py
@@ -4,8 +4,10 @@ from scipy.constants import k as k_B  # Boltzmann constant in J/K
 from scipy.interpolate import interp1d
 from scipy.special import erf
 import WiersmaCopy as Cool
-#from astropy import units as u
-#from astropy.constants import G, k_B, m_p
+from astropy.cosmology import FlatLambdaCDM
+from numba import njit
+
+cosmo = FlatLambdaCDM(H0=70, Om0=0.30)
 
 solarm = 1.988 * 10 ** 33  # solar mass (g)
 meandens = 9.21 * 10 ** (-30)  # 'mean universal density'
@@ -23,46 +25,68 @@ def Ax(n):
     return np.log(1 + n) - (n / (1 + n))
 
 
-def vcgrab(r, rs, R, stelmass, totmass):
-    Rhalf = 3 * ktc * (totmass / 10**12)**(1/3)
-    vc1 = (G * stelmass * solarm) / (r + (Rhalf))
+def findR(z, totmass):
+    rho_m = cosmo.Om(z) * cosmo.critical_density(z)
+    rho_m = rho_m.value
+    x = cosmo.Om(z) - 1
+    delc = 18 * np.pi**2 + 82 * x - 39 * x**2
+    delm = delc / cosmo.Om(z)
+    Rvir = (3 * totmass * solarm / (4 * np.pi * delm * rho_m))**(1/3)
+    return Rvir
+
+
+
+vcgrab_cache = {}
+
+def vcgrab(r, z, stelmass, totmass):
+    key = (r, z, stelmass, totmass)
+
+    if key in vcgrab_cache:
+        return vcgrab_cache[key]
+    
+    R = findR(z, totmass)
+    rs = R / 5
+
+    Rhalf = 3 * ktc * (totmass / 1e12)**(1/3)
+    vc1 = (G * stelmass * solarm) / (r + Rhalf)
+
+    a = 17 * ktc / (1 + np.sqrt(2))
+    vc12 = G * stelmass * solarm * r / (r + a)**2
 
     c = R / rs
     x = r / rs
-    vc2 = (G * (totmass*solarm) * c * Ax(x)) / (R * x * Ax(c))
-    return np.sqrt(vc1 + vc2)
+    vc2 = (G * (totmass * solarm) * c * Ax(x)) / (R * x * Ax(c))
+
+    result = np.sqrt(vc12 + vc2)
+    vcgrab_cache[key] = result
+    return result
 
 
-def Tc(r, rs, R, stelmass, totmass):
+def Tc(r, z, stelmass, totmass):
     const = (0.6 * m_p / (gamma * k_Bcgs))
-    vc2 = vcgrab(r, rs, R, stelmass, totmass)**2
+    vc2 = vcgrab(r, z, stelmass, totmass)**2
     return const * vc2
 
-
+@njit
 def expcor(a):
     return (1.31 * np.exp(a * -.63))/(0.3)
 
 # Global cache for Wiersma cooling object
 _Wiersma_cache = {}
 
+x = np.arange(4.20, 8.20, 0.04)
+y = np.array([-21.6114, -21.4833, -21.5129, -21.5974, -21.6878, -21.7659, -21.8092, -21.8230, -21.8059, -21.7621, -21.6941, -21.6111, 
+    -21.5286, -21.4387, -21.3589, -21.2816, -21.2168, -21.1700, -21.1423, -21.1331, -21.1525, -21.1820, -21.2077, -21.2093, -21.2043, 
+    -21.1937, -21.1832, -21.1811, -21.1799, -21.1883, -21.2263, -21.3118, -21.4700, -21.6521, -21.7926, -21.8728, -21.9090, -21.9290, 
+    -21.9539, -22.0008, -22.0678, -22.1209, -22.1521, -22.1698, -22.1804, -22.1977, -22.2178, -22.2383, -22.2459, -22.2557, -22.2736, 
+    -22.3075, -22.3657, -22.4391, -22.5207, -22.5909, -22.6490, -22.6878, -22.7148, -22.7308, -22.7361, -22.7379, -22.7283, -22.7216, 
+    -22.7102, -22.7023, -22.6962, -22.6921, -22.6959, -22.6994, -22.7050, -22.7170, -22.7249, -22.7378, -22.7480, -22.7629, -22.7710, 
+    -22.7697, -22.7655, -22.7605, -22.7565, -22.7461, -22.7323, -22.7176, -22.7039, -22.6873, -22.6700, -22.6613, -22.6436, -22.6251,
+    -22.6071, -22.5914, -22.5727, -22.5542, -22.5360, -22.5172, -22.5014, -22.4828, -22.4642, -22.4455])
+f = interp1d(x, y, kind='cubic', fill_value='extrapolate')
+
 def Lambdacalc(T, r, assumedZ, n):
     adjT = np.log10(T)
-
-    # Tabulated cooling function (log10(Lambda))
-    x = np.arange(4.20, 8.20, 0.04)
-    y = np.array([
-        -21.6114, -21.4833, -21.5129, -21.5974, -21.6878, -21.7659, -21.8092, -21.8230, -21.8059, -21.7621,
-        -21.6941, -21.6111, -21.5286, -21.4387, -21.3589, -21.2816, -21.2168, -21.1700, -21.1423, -21.1331,
-        -21.1525, -21.1820, -21.2077, -21.2093, -21.2043, -21.1937, -21.1832, -21.1811, -21.1799, -21.1883,
-        -21.2263, -21.3118, -21.4700, -21.6521, -21.7926, -21.8728, -21.9090, -21.9290, -21.9539, -22.0008,
-        -22.0678, -22.1209, -22.1521, -22.1698, -22.1804, -22.1977, -22.2178, -22.2383, -22.2459, -22.2557,
-        -22.2736, -22.3075, -22.3657, -22.4391, -22.5207, -22.5909, -22.6490, -22.6878, -22.7148, -22.7308,
-        -22.7361, -22.7379, -22.7283, -22.7216, -22.7102, -22.7023, -22.6962, -22.6921, -22.6959, -22.6994,
-        -22.7050, -22.7170, -22.7249, -22.7378, -22.7480, -22.7629, -22.7710, -22.7697, -22.7655, -22.7605,
-        -22.7565, -22.7461, -22.7323, -22.7176, -22.7039, -22.6873, -22.6700, -22.6613, -22.6436, -22.6251,
-        -22.6071, -22.5914, -22.5727, -22.5542, -22.5360, -22.5172, -22.5014, -22.4828, -22.4642, -22.4455
-    ])
-    f = interp1d(x, y, kind='cubic', fill_value='extrapolate')
     adjLamb = f(adjT)
     Lambda = 10 ** adjLamb
 
@@ -80,28 +104,28 @@ def Lambdacalc(T, r, assumedZ, n):
         if key not in _Wiersma_cache:
             _Wiersma_cache[key] = Cool.Wiersma_Cooling(Z2Zsun, z)
         cooling = _Wiersma_cache[key]
-        return cooling.LAMBDA(T, n)
+        return cooling.fast_LAMBDA(T, n)
 
     else:
         return Lambda * assumedZ
 
-def dLdTfunc(T):
-    Lambda = Lambdacalc(T)
-    logT = np.log(T)
-    logL = np.log(Lambda)
+# def dLdTfunc(T):
+#     Lambda = Lambdacalc(T)
+#     logT = np.log(T)
+#     logL = np.log(Lambda)
 
-    Lambda2 = Lambdacalc(T * 1.01)
-    logLadj = np.log(Lambda2)
-    logTadj = np.log(T * 1.01)
-    return (logLadj - logL) / (logTadj - logT)
+#     Lambda2 = Lambdacalc(T * 1.01)
+#     logLadj = np.log(Lambda2)
+#     logTadj = np.log(T * 1.01)
+#     return (logLadj - logL) / (logTadj - logT)
 
 
-def dVcdrfunc(r, rs, R, stelmass, totmass):
-    Vc = vcgrab(r, rs, R, stelmass, totmass)
+def dVcdrfunc(r, z, stelmass, totmass):
+    Vc = vcgrab(r, z, stelmass, totmass)
     logvc = np.log(Vc)
     logr = np.log(r)
 
-    logvc2 = np.log(vcgrab(r + 3.0857e15, rs, R, stelmass, totmass))
+    logvc2 = np.log(vcgrab(r + 3.0857e15, z, stelmass, totmass))
     logr2 = np.log(r + 3.0857e15)
     return (logvc2 - logvc) / (logr2 - logr)
 
@@ -111,7 +135,10 @@ def rhocalc(v, tftc, T, r, assumedZ):
     nset = 10**np.arange(-7, 10, 0.01)
     rhoset = nset*mp_g
     Pset = nset * k_Bcgs * T
-    tcoolset = (Pset/(5/2) / (nset**2 * Lambdacalc(T, r, assumedZ, 1)))
+    if assumedZ == "W":
+        tcoolset = (Pset/(5/2) / (nset**2 * Lambdacalc(T, r, "a", 1)))
+    else:
+        tcoolset = (Pset/(5/2) / (nset**2 * Lambdacalc(T, r, assumedZ, 1)))
     vset = (r / (tcoolset * tftc))
 
     lv = np.log10(v)
@@ -126,3 +153,5 @@ def rhocalc(v, tftc, T, r, assumedZ):
     else:
         good_lrho = np.interp(lv, lvs[ind+1:ind-1:-1], log_rhoset[ind+1:ind-1:-1]) 
     return 10.**good_lrho
+
+#Potentially put on HPCC when speed dies.
